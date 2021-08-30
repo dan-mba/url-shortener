80a12a74319c9ae061032d88c334f84a98c76250
Dan <35927536+dan-mba@users.noreply.github.com>
2021-08-30 06:59:14 -0400

    upgrade to mongoose 6

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -8,13 +8,7 @@ let Url: mongoose.Model<mongoose.Document<any>>;
 export async function dbInit()  {
   /* Initialize DB */
   // connect to mongoDB database using mongoose. URL is in .env file for security purposes
-  await mongoose.connect(process.env.MONGO_URI!,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+  await mongoose.connect(process.env.MONGO_URI!);
 
   // Setup Mongoose Schema
   const urlSchema = new mongoose.Schema({
